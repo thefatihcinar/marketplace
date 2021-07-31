31dd9c474623bc16c1688297f89611475bf1829a
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-07-31 21:16:52 +0300

    create main layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      Welcome to the Rocket Store!
+      <Header></Header>
+      <main>
+          <h1>WELCOME TO OUR WEBSITE</h1>
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
