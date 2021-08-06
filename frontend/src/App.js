83fff1b3c87389cf5f7497003e01f80309339948
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-08-06 21:33:46 +0300

    design  footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 /* Import Bootswatch */
 import './bootstrap.min.css' 
+/* Import React-Bootstrap*/
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
 
 
 
@@ -9,10 +12,11 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
-      <main>
+      <main className='pt-1'>
           <h1>WELCOME TO OUR WEBSITE</h1>
       </main>
       <Footer></Footer>
+      <Button variant="primary">Deneme Buton</Button>
     </div>
   );
 }
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
+
 
 const Footer = () => {
     return (
-        <div>
-            FOOTER
-        </div>
+        <footer>
+            <Container>
+                <Row>
+                    <Col className='text-center py-3'>
+                        Copyright Top Charts Filter &copy;
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
     )
 }
 
