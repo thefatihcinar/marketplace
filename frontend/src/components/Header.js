851ef16aa91558025404b2f0ead22e08ce49f99e
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-08-06 21:33:35 +0300

    create Navbar in header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import {Navbar, Nav, Container} from 'react-bootstrap'
+
+const brandName = 'Rocket Store';
 
 const Header = () => {
     return (
        <header>
-           HODOR
+            <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href='/'>{brandName}</Navbar.Brand>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+                    <Navbar.Collapse id='basic-navbar-nav'/>
+                    <Nav className='me-auto'>
+                        <Nav.Link href='/'>
+                            <i className='fas fa-shopping-cart'></i>
+                            Cart
+                        </Nav.Link>
+                        <Nav.Link href='/login'>
+                            <i className='fas fa-user'></i>
+                            Sign In
+                        </Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
        </header>
     )
 }
