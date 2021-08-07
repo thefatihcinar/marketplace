0f52ebffd278bf9fd3363b79ff593e2b2cc2fe58
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-08-07 21:00:46 +0300

    bug fixed in Rating component, rename bug

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,39 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-let rating;
-
 const Rating = ({ rating, numReviews, color }) => {
     return (
         <div className = 'rating'>
             <div className='ml-1'></div>
             <span>
-               <i style = {{color: color}} className = { value >= 1 ? 'fas fa-star':
-                    value >= 0.5 ? 'fas fa-star-half-alt': 'far fa-star'
+               <i style = {{color: color}} className = { rating >= 1 ? 'fas fa-star':
+                    rating >= 0.5 ? 'fas fa-star-half-alt': 'far fa-star'
                }>
                </i>
             </span>
             <span>
-               <i style = {{color: color}} className = { value >= 2 ? 'fas fa-star':
-                    value >= 1.5 ? 'fas fa-star-half-alt': 'far fa-star'
+               <i style = {{color: color}} className = { rating >= 2 ? 'fas fa-star':
+                    rating >= 1.5 ? 'fas fa-star-half-alt': 'far fa-star'
                }>
                </i>
             </span>
             <span>
-               <i style = {{color: color}} className = { value >= 3 ? 'fas fa-star':
-                    value >= 2.5 ? 'fas fa-star-half-alt': 'far fa-star'
+               <i style = {{color: color}} className = { rating >= 3 ? 'fas fa-star':
+                    rating >= 2.5 ? 'fas fa-star-half-alt': 'far fa-star'
                }>
                </i>
             </span>
             <span>
-               <i style = {{color: color}} className = { value >= 4 ? 'fas fa-star':
-                    value >= 3.5 ? 'fas fa-star-half-alt': 'far fa-star'
+               <i style = {{color: color}} className = { rating >= 4 ? 'fas fa-star':
+                    rating >= 3.5 ? 'fas fa-star-half-alt': 'far fa-star'
                }>
                </i>
             </span>
             <span>
-               <i style = {{color: color}} className = { value >= 5 ? 'fas fa-star':
-                    value >= 4.5 ? 'fas fa-star-half-alt': 'far fa-star'
+               <i style = {{color: color}} className = { rating >= 5 ? 'fas fa-star':
+                    rating >= 4.5 ? 'fas fa-star-half-alt': 'far fa-star'
                }>
                </i>
             </span>
