1a28236b794d2930341051f77b87ab18d3cd2a6f
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-08-17 15:36:23 +0300

    Switch Links in Header component from a tag to LinkContainer

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Navbar, Nav, Container} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom'
 
 const brandName = 'Rocket Store';
 
@@ -8,18 +10,24 @@ const Header = () => {
        <header>
             <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href='/'>{brandName}</Navbar.Brand>
+                    <LinkContainer to = '/'>
+                        <Navbar.Brand>{brandName}</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls='basic-navbar-nav'/>
                     <Navbar.Collapse id='basic-navbar-nav'/>
                     <Nav className='me-auto'>
-                        <Nav.Link href='/'>
-                            <i className='fas fa-shopping-cart'></i>
-                            Cart
-                        </Nav.Link>
-                        <Nav.Link href='/login'>
-                            <i className='fas fa-user'></i>
-                            Sign In
-                        </Nav.Link>
+                        <LinkContainer to = '/cart'>
+                            <Nav.Link>
+                                <i className='fas fa-shopping-cart'></i>
+                                Cart
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to = '/login'>
+                            <Nav.Link>
+                                <i className='fas fa-user'></i>
+                                Sign In
+                            </Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Container>
             </Navbar>
