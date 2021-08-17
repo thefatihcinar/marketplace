76efb049b0f819dc185a74c397008a295a92c70a
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-08-17 15:36:57 +0300

    Switch Links from a tag into Link / LinkContainer in ProductsScreen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import { Link } from 'react-router-dom'
 import Rating from './Rating'
 
 const Product = ({ product }) => {
     return (
         <Card className='my-3 p-3 rounded'>
-            <LinkContainer to = {`/product/${product._id}`}>
+            <Link to = {`/product/${product._id}`}>
                 <Card.Img src={product.image} variant='top'/>
-            </LinkContainer>
+            </Link>
             <Card.Body as = 'div'>
-                <LinkContainer href = {`/product/${product._id}`}>
+                <Link to = {`/product/${product._id}`}>
                     <Card.Title style = {{color:'black'}}>
                         <strong>{product.name}</strong>
                     </Card.Title>
-                </LinkContainer>
+                </Link>
                 <Card.Text as = 'div'>
                     <div className = 'mt-3'>
                         <Rating rating = {product.rating} 
