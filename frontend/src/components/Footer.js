ae6c5bd58e2082404cc7c096c3f42e3322b1eebd
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-08-17 14:29:36 +0300

    rename copyright again

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
             <Container>
                 <Row>
                     <Col className='text-center py-3'>
-                        Copyright &copy Rocket Store ;
+                        Copyright &copy; Rocket Store 
                     </Col>
                 </Row>
             </Container>
