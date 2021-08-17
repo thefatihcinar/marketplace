ecc5b491253011e07739dc489b37d6c4a9361f18
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-08-17 15:37:05 +0300

    create ProductDetails component

--- frontend/src/components/ProductDetails.js
+++ frontend/src/components/ProductDetails.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const ProductDetails = ({ product }) => {
     return (
-        <div>
-            
-        </div>
+        <strong>
+            Product Details
+        </strong>
     )
 }
 
