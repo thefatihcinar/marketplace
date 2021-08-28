b9c2b113fc4daf62120f28457a4b9ebb09a6de12
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-08-28 21:35:11 +0300

    add /api to routes

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,12 @@ app.listen(5000, console.log('Server is running on port 5000'));
 app.get("/", (request, response) => (response.send("The server is running.")));
 
 /* Fetch all products */
-app.get("/products", (request, response) => (
+app.get("/api/products", (request, response) => (
     response.json(products)
 ));
 
 /* Fetch a specific product */
-app.get("/products/:id", (request, response) => {
+app.get("/api/products/:id", (request, response) => {
     let productId = request.params.id;
     let product = products.find( p => p._id == productId);
     response.json(product);
