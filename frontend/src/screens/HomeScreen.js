529b34f6b627c3443710b991d008374c02e8e344
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-08-28 15:54:48 +0300

    assign a key prop to each product in HomeScreen component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -10,11 +10,11 @@ const HomeScreen = () => {
                 <h1 className='mt-3'>Latest Products</h1>
                 <Row>
                     {
-                        products.map(product => (
-                            <Col sm={12} md={6} lg={4} xl={3}>
-                                <Product product = {product}></Product>
-                            </Col>
-                        ))
+                    products.map(product => (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            <Product product = {product}></Product>
+                        </Col>
+                    ))
                     }
                 </Row>
             </Container>
