95bf1af5c3c025c6cffff39cb421fbb426b7ac0b
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-08-28 18:59:18 +0300

    add code to fetch all products in backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
-const express = require('express');
+//CommonJS syntax: const express = require('express');
+import express from 'express'
+import products from './data/products.js'
 
 const app = express();
 
 app.listen(5000, console.log('Server is running on port 5000'));
 
-app.get("/", (request, response) => (response.send("Hello")));
+app.get("/", (request, response) => (response.send("The server is running.")));
+
+/* Fetch all products */
+app.get("/products", (request, response) => (
+    response.json(products)
+));
+
