86dfca0113f4ab916a4bc356097cdd370ab779ee
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-08-29 23:08:53 +0300

    connect to the database when starting the server

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 //CommonJS syntax: const express = require('express');
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import products from './data/products.js'
+import connectDB from './configurations/database.js'
+
 
 dotenv.config();
+
+/* connect to the MongoDB first */
+connectDB()
 
 const app = express();
 
 const PORT = process.env.PORT;
 
-app.listen(PORT || 5000, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT || 5000, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
 app.get("/", (request, response) => (response.send("The server is running.")));
 
