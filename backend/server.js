deb3bc54eb45547baa119e3482a2e96d98ba62a5
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-08-30 22:56:14 +0300

    create custom error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -23,3 +23,19 @@ app.get("/", (request, response) => (response.send("The server is running.")));
 
 /* Use Routers */
 app.use("/api/products", productRouter);
+
+
+/* Override the default Error Handler Middleware */
+app.use((err, request, response, next) => {
+    /* the custom error handler middleware, responsible for when there is an error */
+    /* decide the status code */
+    let statusCode = response.statusCode === 200 ? 500 : response.statusCode;
+    // make 200 status codes, 500 
+    response.status(statusCode);
+    // return the error
+    response.json( {
+        message: err.message,
+        stack: process.env.NODE_ENV == 'production' ? null : err.stack
+    });
+
+});
