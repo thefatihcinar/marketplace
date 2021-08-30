ca4fc4fc59ebf670d243c986ccf950ef505c3240
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-08-30 22:08:23 +0300

    use asyncHandler in productRouter for errors

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,7 @@
 /* all the routes that start with /api/products will be forwarded 
 to this router by the server.js i.e. main app */
 import express from 'express'
+import asyncHandler from 'express-async-handler'
 import Product from '../models/productModel.js'
 
 const router = express.Router()
@@ -8,18 +9,18 @@ const router = express.Router()
 // @desc    get all the products
 // @route   GET /api/products
 // @access  Public 
-router.get("/", async (request, response) => {
+router.get("/", asyncHandler (async (request, response) => {
     /* This function will return all the products */
 
     let products = await Product.find({})
 
     response.json(products)
-});
+}));
 
 // @desc    get a single product by id
 // @route   GET /api/products/:id
 // @access  Public
-router.get("/:id", async (request, response) => {
+router.get("/:id", asyncHandler (async (request, response) => {
     /* this function will return a single product */
 
     let productId = request.params.id
@@ -34,7 +35,7 @@ router.get("/:id", async (request, response) => {
         response.status(404).json( { message: "product not found"} )
     }
 
-});
+}));
 
 
 export default router
