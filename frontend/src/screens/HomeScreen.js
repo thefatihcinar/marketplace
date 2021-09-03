4b89860dda9ebf9ca359b5378a338922635aabc4
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-03 12:59:50 +0300

    consider the cases of loading, error and success in HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -21,15 +21,19 @@ const HomeScreen = () => {
         <div>
             <Container>
                 <h1 className='mt-3'>Latest Products</h1>
-                <Row>
+                {loading ? (<h3>LOADING</h3>)
+                :error ? (<h1>error</h1>)
+                :(<Row>
                     {
                     products.map(product => (
                         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                             <Product product = {product}></Product>
                         </Col>
-                    ))
+                     ))
                     }
-                </Row>
+                </Row>)
+                }
+                
             </Container>
         </div>
     )
