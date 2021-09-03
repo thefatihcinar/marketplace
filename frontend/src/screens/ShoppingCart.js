879754b67f6bcf85e23b44daeb014c3f95473c48
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-03 18:27:04 +0300

    create Shopping Cart component

---
+++ frontend/src/screens/ShoppingCart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShoppingCart = () => {
+    return (
+        <div>
+            <span> THIS IS SHOPPING CART</span>
+        </div>
+    )
+}
+
+export default ShoppingCart
