8f2a4c95fcc2a470bd9d6b50e6db45892a3b65df
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-04 20:22:16 +0300

    create addToCart action

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
+
+export const addToCart = (productId, quantity) => async(dispatch) => {
+    /* this action creator adds a new item to the shopping cart
+       it connects to the API to thet the product details and returns
+       actions to add to the global state */
+    
+    const { data } = await axios.get(`/api/products/${productId}`);
+
+    /* create the item object */
+
+    let item = {
+        product: data._id,
+        name: data.name,
+        image: data.image,
+        price: data.price,
+        countInStock: data.countInStock,
+        quantity: quantity
+    };
+
+    /* return the action */
+    dispatch( {
+        type: CART_ADD_ITEM,
+        payload : item 
+    } )
+
+};
