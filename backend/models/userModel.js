e85716058fdde87563028e458118b623b65a160c
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-08 18:38:47 +0300

    mongoose.pre, hash password right before save

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -16,6 +16,27 @@ userSchema.methods.matchPassword = async function(enteredPassword){
     return await bcrypt.compare(enteredPassword, this.password);
 }
 
+userSchema.pre('save', async function(next){
+    /* this piece of mongoose middleware will
+       hash the password just before creating the user */
+
+    if(!this.isModified('password')){
+        // if the password is not modified do not do anything again
+        // definitely do not hash the password again, NEVER
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.post('save', function(createdDocument, next){
+    /* this piece of mongoose middleware will
+        warn the console when a new user registers */
+    console.log(createdDocument);
+    next();
+})
+
 /* create a model, i.e. User model from this schema */
 const User = mongoose.model('User', userSchema);
 
