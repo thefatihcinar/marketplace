b105e0875b3d8d6fcb1b127403211f247be86b12
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-08 13:46:24 +0300

    map routes and controllers, use controller actions in routes for Product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,42 +1,13 @@
 /* all the routes that start with /api/products will be forwarded 
 to this router by the server.js i.e. main app */
 import express from 'express'
-import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
+/* Actions from Controller */
+import { listProducts, getProduct } from '../controllers/productController.js'
 
 const router = express.Router()
 
-// @desc    get all the products
-// @route   GET /api/products
-// @access  Public 
-router.get("/", asyncHandler (async (request, response) => {
-    /* This function will return all the products */
+router.route("/").get(listProducts);
 
-    let products = await Product.find({})
-
-    response.json(products)
-}));
-
-// @desc    get a single product by id
-// @route   GET /api/products/:id
-// @access  Public
-router.get("/:id", asyncHandler (async (request, response) => {
-    /* this function will return a single product */
-
-    let productId = request.params.id
-
-    let product = await Product.findById(productId)
-
-    if(product) {
-        response.json(product)
-    }
-    else{
-        // product not found
-        response.status(404)
-        throw new Error('product not found')
-    }
-
-}));
-
+router.route("/:id").get(getProduct);
 
 export default router
