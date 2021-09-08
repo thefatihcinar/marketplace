61cf08c7b0b70944ca7d707506d463bf899300a5
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-08 16:12:24 +0300

    use protect middleware in getUser route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,11 +3,14 @@ import express from 'express'
 /* Import Actions from Controller */
 import { authUser, getUserProfile } from '../controllers/userController.js';
 
+/* Import Middlewares */
+import { protect } from '../middleware/authMiddleware.js'
+
 const router = express.Router();
 
 // POST /api/users/login/
 router.route("/login").post(authUser);
 // GET /api/users/profile
-router.route("/profile").get(getUserProfile);
+router.route("/profile").get(protect, getUserProfile);
 
 export default router
