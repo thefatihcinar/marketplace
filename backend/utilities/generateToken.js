2a01ca214875e3bd1b4639425d22a9f9e8d7fe82
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-08 15:30:58 +0300

    [feature] generate token

---
+++ backend/utilities/generateToken.js
@@ -0,0 +1,9 @@
+import jwt from 'jsonwebtoken'
+
+const generateToken = (id) => {
+    /* this function will create a JSON Web Token 
+       with the payload containing the user id */
+    return jwt.sign({id: id}, process.env.JWT_SECRET, { expiresIn: '1s'});
+}
+
+export default generateToken;
