961345f9eec3839873a9473e62b6698fdb82e73e
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-08 15:08:16 +0300

    add matchPassword method into User model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema(
     {
@@ -9,8 +10,13 @@ const userSchema = mongoose.Schema(
     },
     { timestamps : true });
 
-/* create a model, i.e. User model from this schema */
+/* also create methods that run on this model */
 
+userSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+/* create a model, i.e. User model from this schema */
 const User = mongoose.model('User', userSchema);
 
 export default User
