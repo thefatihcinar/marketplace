e9cd5057cde5f842c0f4aba081c8b7995ce13120
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-08 15:08:46 +0300

    [feature] user login / authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,9 +11,36 @@ const authUser = asyncHandler( async (request, response) => {
     /* this function is responsible for authenticating the user 
         i.e. LOG IN */
     
-    console.log(request.body)
+    let { email, password } = request.body;
+    // destructure email and password from requst body, which is parsed
+   
+    // To-do: Add Null Check
+    if(email === undefined || password === undefined)
+        throw new Error("email and password must be provided");
 
-    response.send("ok");
+    // Go find the user in the db
+    const user = await User.findOne({ email: email });
+
+    if(user && await user.matchPassword(password)){
+        // user is authenticated
+        response.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: null
+        });
+    }
+    else if (user){
+        // user exists but password is incorrent
+        response.status(401)
+        throw new Error("password is incorrect");
+    }
+    else {
+        // the user does not exist
+        response.status(401)
+        throw new Error("user not found");
+    }
 
 })
 
