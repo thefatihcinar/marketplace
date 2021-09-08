f597a3d528f888a580d5a82c56c7494d05c47d4c
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-08 15:54:00 +0300

    update the expiration dates of the tokens to 2 minutes

--- backend/utilities/generateToken.js
+++ backend/utilities/generateToken.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 const generateToken = (id) => {
     /* this function will create a JSON Web Token 
        with the payload containing the user id */
-    return jwt.sign({id: id}, process.env.JWT_SECRET, { expiresIn: '1s'});
+    return jwt.sign({id: id}, process.env.JWT_SECRET, { expiresIn: '2m'});
 }
 
 export default generateToken;
