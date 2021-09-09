8220989bfd5b35f6f841415de4173f2776dfa232
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-09 11:29:55 +0300

    create FormContainer component for reusability

---
+++ frontend/src/components/FormContainer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const FormContainer = ( { children } ) => {
+    return (
+        <Container>
+            <Row className='justify-content-md-centered'>
+                <Col sm={12} md={6}>
+                    {children}
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default FormContainer
