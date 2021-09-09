956486fea54080089eab297af70215bcce1f10ed
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-09 11:30:26 +0300

    route for login component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,14 @@ import HomeScreen from './screens/HomeScreen'
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 import ProductDetails from './components/ProductDetails'
 import ShoppingCart from './screens/ShoppingCart'
+import Login from './components/Login'
 
 function App() {
   return (
     <Router>
       <Header></Header>
       <Route path = '/' component = {HomeScreen} exact></Route>
+      <Route path = '/login' component = {Login}></Route>
       <Route path = '/product/:id' component = {ProductDetails} ></Route>
       <Route path = '/cart/:id?' component = {ShoppingCart}></Route>
       <Footer></Footer>
