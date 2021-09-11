6965e1b63df8e14797e831cf42bd41f3c89cbca5
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-11 18:24:35 +0300

    make tokens last for 10minutes

--- backend/utilities/generateToken.js
+++ backend/utilities/generateToken.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 const generateToken = (id) => {
     /* this function will create a JSON Web Token 
        with the payload containing the user id */
-    return jwt.sign({id: id}, process.env.JWT_SECRET, { expiresIn: '2m'});
+    return jwt.sign({id: id}, process.env.JWT_SECRET, { expiresIn: '10m'});
 }
 
 export default generateToken;
