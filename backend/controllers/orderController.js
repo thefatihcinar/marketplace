a53063f1e3e63ccbe309e314dbe97435c0db8e07
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-19 11:12:38 +0300

    create order controler

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,7 @@
+import asyncHandler from 'express-async-handler'
+import Order from '../models/orderModel.js'
+import Product from '../models/productModel.js'
+
+const createOrder = asyncHandler( async (request, response) => {
+
+})
