574bcbcacdb2879715c81cb840f6b502d2f6d323
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-19 11:13:49 +0300

    add decriptions to order controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,6 +2,9 @@ import asyncHandler from 'express-async-handler'
 import Order from '../models/orderModel.js'
 import Product from '../models/productModel.js'
 
+// @desc    create new order
+// @route   POST /api/orders
+// @access  private
 const createOrder = asyncHandler( async (request, response) => {
 
 })
