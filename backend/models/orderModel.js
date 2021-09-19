b2699d7acb7524650575e448d68f3b1ff20410f5
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-19 13:10:33 +0300

    add items price to order model

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -25,6 +25,7 @@ const orderSchema = mongoose.Schema(
             update_time: { type: String },
             email_address: { type: String}
         },
+        "itemsPrice": { type: Number, required: true, default: 0.0 },
         "taxPrice": { type: Number, required: true, default: 0.0 },
         "shippingPrice": { type: Number, required: true, default: 0.0 },
         "totalPrice": { type: Number, required: true, default: 0.0 },
