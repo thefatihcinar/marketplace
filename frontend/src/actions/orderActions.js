dfde2ad0df5ca05231c3712ac5bbd6259d89eb40
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-19 12:18:42 +0300

    create order action creator

---
+++ frontend/src/actions/orderActions.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+import {
+    ORDER_CREATE_REQUEST,
+    ORDER_CREATE_SUCCESS,
+    ORDER_CREATE_FAILURE
+} from '../constants/orderConstants.js'
+
+export const createOrder = (order) => async(dispatch, getState) =>  {
+    /* this action creator is responsible of communicating 
+        with the API and creating an order */
+    
+    try {
+        /* First get the logged in user in order to access token */
+        const { userLogin: { userInfo } } = getState();
+    
+        dispatch( { type: ORDER_CREATE_REQUEST } );
+    
+        let config = {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${userInfo.token}`
+            }
+        }
+    
+        let { data } = await axios.post("/api/orders", order, config);
+    
+        dispatch( { type: ORDER_CREATE_SUCCESS, payload: data} );
+
+    } catch (error) {
+        
+        dispatch( { type: ORDER_CREATE_FAILURE,
+                    payload: error.response && error.response.data.message
+                             ? error.response.data.message
+                             : error.message} )
+    }
+}
+
