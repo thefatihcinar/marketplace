05e43a898601397daf0052a48041f24cc115e766
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-19 11:14:48 +0300

    parse request body and validate order items, shipping address and payment method in order controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,5 +6,36 @@ import Product from '../models/productModel.js'
 // @route   POST /api/orders
 // @access  private
 const createOrder = asyncHandler( async (request, response) => {
-
+    /* this controller action is responsible for adding a new order
+       to the database */
+    
+    let {   
+        orderItems,
+        shippingAddress,
+        paymentMethod
+      } = request.body;
+  
+      if(!orderItems){
+          response.status(400);
+          throw new Error("there are no valid order items");
+          return;
+      }
+  
+      if(!shippingAddress){
+          response.status(400);
+          throw new Error("shipping address is not provided");
+          return;
+      }
+  
+      if(!paymentMethod){
+          response.status(400);
+          throw new Error("payment method is not provided");
+          return;
+      }
+  
+      if(orderItems && orderItems.length === 0){
+          response.status(400);
+          throw new Error("no order items");
+          return;
+      }
 })
